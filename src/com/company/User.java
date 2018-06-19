aed65e73392eb46f7dab621d04ea3b851f492535
damien <dubois.damien508@gmail.com>
2018-06-19 10:53:16 +0200

    first commit

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -23,32 +23,4 @@ class User {
         this.login = userlogin;
         this.password = userpassword;
     }
-
-    public static void main (String [] args) {
-
-        String login = "air";
-        String password = "guitare";
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Veullez saisir votre login ");
-        String str = sc.nextLine();
-        System.out.println("Votre login est : " + str);
-
-        if (str.equals(login)) {
-            System.out.println("C'est le bon login");
-        } else {
-            System.out.println("Ce n'est pas  le bon login"); }
-
-        System.out.println("Veullez saisir votre mot de passe ");
-        String str2 = sc.nextLine();
-        System.out.println("Votre mot de passe est : " + str2);
-
-        if (str2.equals(password)) {
-            System.out.println("C'est le bon mot de passe");
-        } else {
-            System.out.println("Ce n'est pas le bon mot de passe"); }
-
-       // return expected
-    }
-
 }
