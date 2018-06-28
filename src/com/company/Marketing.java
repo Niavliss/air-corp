a32655cd4856befbfbcb69c24b9ab0ea8aaa1cb5
Niavliss <sylvain.felix@gmail.com>
2018-06-28 11:36:38 +0200

    Main and Marketing updated

--- src/com/company/Marketing.java
+++ src/com/company/Marketing.java
@@ -9,6 +9,7 @@ public class Marketing extends User {
     public Marketing(String login, String password) {
 
         super(login, password);
+        catalog = new Catalog();
 
     }
 
@@ -16,7 +17,7 @@ public class Marketing extends User {
         super.displayMenu();
         System.out.println("Choisir 3 pour afficher la liste des produits");
         System.out.println("Choisir 4 pour modifier le prix d'un produit");
-        System.out.println("Choisir 9 pour sortir ");
+        System.out.println("Choisir 9 pour sortir");
     }
 
     public void exec(int reponse) {
@@ -53,11 +54,10 @@ public class Marketing extends User {
         Scanner sc1 = new Scanner(System.in);
         System.out.println("Veuillez modifier le prix de l'article");
 
-        Float priceProduct = sc.nextFloat();
+        Float priceProduct = sc1.nextFloat();
         catalog.updatePrice(idProduct,priceProduct);
 
-
-
+        System.out.println(this.catalog);
 
     }
 
