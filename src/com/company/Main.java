3aba3e5da87408f1128e187bd96e8135a6e67f21
Niavliss <sylvain.felix@gmail.com>
2018-07-03 11:50:51 +0200

    exception menu added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import com.company.profils.Customer;
 import com.company.profils.Marketing;
 import com.company.profils.User;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -65,10 +66,17 @@ public class Main {
             customer = new Customer();
         }
 
+        try {
         customer.displayMenu();
         int reponse = sc.nextInt();
 
         customer.exec(reponse);
+
+        } catch (InputMismatchException err) {
+
+            System.out.println("Tu n'as pas choisi de Chiffre ! Dehors !!");
+
+        }
 
     }
 
